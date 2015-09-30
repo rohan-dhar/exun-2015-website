5f7d01446ba713facc22a3a692a90540b6a3bb39
Sana Gujral <thesanagujral@gmail.com>
2015-10-01 00:05:26 +0530

    Update members.php

--- members.php
+++ members.php
@@ -25,13 +25,13 @@
 	
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Devansh Gandhi</h3>
+					<h3 class="memName">Akshay Gupta</h3>
 					<h3 class="memDesig">President</h3>
 				</div>
-
+				
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Akshay Gupta</h3>
+					<h3 class="memName">Devansh Gandhi</h3>
 					<h3 class="memDesig">President</h3>
 				</div>
 
@@ -44,37 +44,59 @@
 					<h3 class="memName">Harshil Kashyap</h3>
 					<h3 class="memDesig">Secretray, Exun</h3>
 				</div>
-
-
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
 					<h3 class="memName">Rishabh Jain</h3>
 					<h3 class="memDesig">Head, Cryptic Hunts</h3>
 				</div>
-
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
 					<h3 class="memName">Shikhar Shivam</h3>
 					<h3 class="memDesig">Head, Exun Hardware</h3>
 				</div>
-
-
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Rishabh Raj Narula</h3>
-					<h3 class="memDesig">Sr. Member</h3>
+					<h3 class="memName">Abhimanyu Sinha</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
 				</div>
-
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Abhinav Chadha</h3>
-					<h3 class="memDesig">Sr. Member</h3>
-				</div>
-
-
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
 					<h3 class="memName">Abhishek Anand</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Anumay Mishra</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>			
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Arkin Gupta</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Neel Bakshi</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Rahul Shekhar</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Sambhav Anand</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Sana Gujral</h3>
+					<h3 class="memDesig">Honorary Advisor</h3>
+				</div>
+				<div class="memCard">
+					<img class="memPic" src='IMG/user.png' />
+					<h3 class="memName">Shubham Aggarwal</h3>
 					<h3 class="memDesig">Honorary Advisor</h3>
 				</div>
 				<div class="memCard">
@@ -87,49 +109,15 @@
 					<h3 class="memName">Zorawar Singh</h3>
 					<h3 class="memDesig">Honorary Advisor</h3>
 				</div>
-				
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Anumay Mishra</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-				
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Shubham Aggarwal</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Arkin Gupta</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Rahul Shekhar</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
+					<h3 class="memName">Abhinav Chadha</h3>
+					<h3 class="memDesig">Sr. Member</h3>
 				</div>
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Abhimanyu Sinha</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Sana Gujral</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Neel Bakshi</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
-				</div>
-				<div class="memCard">
-					<img class="memPic" src='IMG/user.png' />
-					<h3 class="memName">Sambhav Anand</h3>
-					<h3 class="memDesig">Honorary Advisor</h3>
+					<h3 class="memName">Rishabh Raj Narula</h3>
+					<h3 class="memDesig">Sr. Member</h3>
 				</div>
 				<div class="memCard">
 					<img class="memPic" src='IMG/user.png' />
